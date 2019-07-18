98425eac29a164de7484e415df1d1c63d961c9cf
Maksym Marko <maximus21@mail.ru>
2019-07-18 10:15:17 +0300

    FETCH_POSTS action

---
+++ src/components/Posts.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
+
+class Posts extends Component {
+
+	componentWillMount() {
+		this.props.fetchPosts();
+	}
+
+	render() {
+
+		const items = this.props.posts.map( post => (
+			<div key={post.id}>
+				<h3>{post.title}</h3>
+				<p>{post.body}</p>
+			</div>
+		) );
+
+		return(
+			<div>
+				{items}
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => ( {
+	posts: state.posts.items
+} );
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
